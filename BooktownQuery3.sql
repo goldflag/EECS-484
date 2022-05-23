f418a89cd366923af4af099bd681cdb37a87baf4
rbederka <47360495+rbederka@users.noreply.github.com>
2022-05-22 20:27:15 -0400

    Delete BooktownQuery3.sql

--- BooktownQuery3.sql
+++
@@ -1,7 +0,0 @@
-SELECT E.ISBN 
-From Editions E, Books B, Authors A
-WHERE E.Book_id = B.Book_id 
-AND B.author_id = A.author_id 
-AND A.First_name = 'Agatha' 
-AND A.Last_name = 'Christie'
-ORDER BY ISBN DESC;
