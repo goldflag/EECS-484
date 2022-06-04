752bc04cb61d28459e3aa236b6da50d9c437fd74
rbederka <47360495+rbederka@users.noreply.github.com>
2022-06-04 10:46:44 -0400

    Update query1.js

--- P3_starter_code/query1.js
+++ P3_starter_code/query1.js
@@ -4,6 +4,7 @@ function find_user(city, dbname){
     db = db.getSiblingDB(dbname);
     var results = [];
    
+    //Find hometown city who matches city and return user_id with it
     
     results = db.users.find({"hometown.city" : city}, {user_id: 1, _id: 0});
     
