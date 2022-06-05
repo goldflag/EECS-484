07563c702686aedd54716ec512b54eb34db1dee9
rbederka <47360495+rbederka@users.noreply.github.com>
2022-06-05 11:41:33 -0500

    Update query3.js

--- project3/query3.js
+++ project3/query3.js
@@ -10,7 +10,10 @@
 
 function cities_table(dbname) {
     db = db.getSiblingDB(dbname);
-    // TODO: implemente cities collection here
+   
+  //Use aggregate and group to set id to cities and group users together by city. Then make array of users push back user_IDs of those who live in city
+  //Next output using $out to a collection called "cities
+  db.users.aggregate( [ { $group: { _id: "$hometown.city", users: { $push: "$user_id"} } }, {$out: "cities"} ] );
 
 
     // Returns nothing. Instead, it creates a collection inside the datbase.
