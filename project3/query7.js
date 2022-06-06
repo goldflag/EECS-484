51d78082a42c0bd90ee3fb3a9b9099640de7d569
rbederka <47360495+rbederka@users.noreply.github.com>
2022-06-06 13:23:19 -0400

    Update query7.js

--- project3/query7.js
+++ project3/query7.js
@@ -1,11 +1,18 @@
 // query 7: Find the number of users born in each month using MapReduce
 
+
+//Website below for help
+//https://www.mongodb.com/docs/manual/tutorial/map-reduce-examples/
+
 var num_month_mapper = function() {
   // Implement the map function
+  emit(this.MOB, this.users.length)
+  
 }
 
 var num_month_reducer = function(key, values) {
   // Implement the reduce function
+  return Array.sum(values);
 }
 
 var num_month_finalizer = function(key, reduceVal) {
