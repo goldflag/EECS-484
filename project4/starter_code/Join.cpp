55af9642fc42e2f4d56f816128543ea9ac0582fe
Bill Yang <billyang@umich.edu>
2022-06-12 22:54:14 -0400

    fix partition

--- project4/starter_code/Join.cpp
+++ project4/starter_code/Join.cpp
@@ -3,45 +3,46 @@
 // #include "P.hpp"
 #include <functional>
 
-int mod_const = MEM_SIZE_IN_PAGE - 1;
-int input_buffer = 15;
+unsigned int mod_const = MEM_SIZE_IN_PAGE - 1;
+unsigned int input_buffer = 15;
 
 
 
 void loadRef(
     Disk* disk, 
     Mem* mem, 
-    vector<Bucket>* buckets,
+    vector<Bucket>& buckets,
     bool left, 
-    int start,
-    int end
+    unsigned int start,
+    unsigned int end
 );
 
 
 void loadRef(
     Disk* disk, 
     Mem* mem, 
-    vector<Bucket> buckets,
+    vector<Bucket>& buckets,
     bool left, 
-    int start,
-    int end
+    unsigned int start,
+    unsigned int end
 ) {
-    cout << "===== LOADING REF ===== \n";
+    std::cout << "===== LOADING REF ===== \n";
+    std::cout << "FROM " << start << " TO " << end << "\n";
     Page* input_buffer_page = mem->mem_page(input_buffer);
-    for (int page_id = start; page_id < end; page_id++) {
+    for (unsigned int page_id = start; page_id < end; page_id++) {
         mem->loadFromDisk(disk, page_id, input_buffer);
         // input_buffer_page->print();
 
-        for (int record_idx = 0; record_idx < input_buffer_page->size(); ++record_idx) {
+        for (unsigned int record_idx = 0; record_idx < input_buffer_page->size(); ++record_idx) {
             Record current_record = input_buffer_page->get_record(record_idx);
             current_record.print();
-            cout << current_record.partition_hash() % mod_const << "\n"; 
-            int partition_index = current_record.partition_hash() % mod_const;
+            std::cout << current_record.partition_hash() % mod_const << "\n"; 
+            unsigned int partition_index = current_record.partition_hash() % mod_const;
             Page* partition_buffer_page = mem->mem_page(partition_index);
             partition_buffer_page->loadRecord(current_record);
             if (partition_buffer_page->full()) {
-                int disk_page_id = mem->flushToDisk(disk, partition_index);
-                cout << "flushed to: " << disk_page_id << "\n"; 
+                unsigned int disk_page_id = mem->flushToDisk(disk, partition_index);
+                std::cout << "flushed to: " << disk_page_id << "\n"; 
                 if (left) {
                     buckets[partition_index].add_left_rel_page(disk_page_id);
                 } else {
@@ -49,6 +50,20 @@ void loadRef(
                 }
             }
         }
+    }
+
+    for (unsigned int partition_index = 0; partition_index < MEM_SIZE_IN_PAGE - 1; partition_index++) {
+        Page* partition_buffer_page = mem->mem_page(partition_index);
+        if (partition_buffer_page->size() > 0) {
+            cout << "Wrote " << partition_index << " to disk\n";
+            unsigned int disk_page_id = mem->flushToDisk(disk, partition_index);
+            if (left) {
+                buckets[partition_index].add_left_rel_page(disk_page_id);
+            } else {
+                buckets[partition_index].add_right_rel_page(disk_page_id);
+            }
+        }
+
     }
 }
 
@@ -65,23 +80,22 @@ vector<Bucket> partition(
     pair<unsigned int, unsigned int> right_rel) 
 {
     vector<Bucket> buckets;
-    for (int i = 0; i < MEM_SIZE_IN_PAGE - 1; i++) {
+    for (unsigned int i = 0; i < MEM_SIZE_IN_PAGE - 1; i++) {
         buckets.push_back(Bucket(disk));
     }
 
-    const int left_start = std::get<0>(left_rel);
-    const int left_end = std::get<1>(left_rel);
-    const int right_start = std::get<0>(right_rel);
-    const int right_end = std::get<1>(right_rel);
+    const unsigned int left_start = std::get<0>(left_rel);
+    const unsigned int left_end = std::get<1>(left_rel);
+    const unsigned int right_start = std::get<0>(right_rel);
+    const unsigned int right_end = std::get<1>(right_rel);
 
     loadRef(disk, mem, buckets, true, left_start, left_end);
-    mem->reset();
+    // mem->reset();
     loadRef(disk, mem, buckets, false, right_start, right_end);
 
-
-    // std::cout << "memsize: " << MEM_SIZE_IN_PAGE << "\n";
-    std::cout << '(' << left_start << ", " << left_end << ")\n";
-    std::cout << '(' << right_start << ", " << right_end << ")\n";
+    for (unsigned int i = 0; i < MEM_SIZE_IN_PAGE - 1; i++) {
+        std::cout << i << ", right: " << buckets[i].num_left_rel_record << ", left: " << buckets[i].num_right_rel_record << "\n";
+    }
 
     return buckets;
 }
